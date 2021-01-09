b970ddac4a12637add547041fd8ed9afe1a31031
Alex <alexghita96@gmail.com>
2021-01-09 22:20:26 +0000

    style: style cleanup, replace boilerplate with Lombok annotations

--- src/main/java/obscurum/environment/background/BackgroundTile.java
+++ src/main/java/obscurum/environment/background/BackgroundTile.java
@@ -2,40 +2,17 @@ package obscurum.environment.background;
 
 import java.awt.Color;
 
+import lombok.Getter;
+import lombok.NonNull;
 import obscurum.environment.Tile;
 
-/**
- * This models a background tile, which is usually a floor tile or something
- * similar on which foreground tiles can be placed.
- * @author Alex Ghita
- */
+@Getter
 public abstract class BackgroundTile extends Tile {
-  private Color backgroundColour;
+  private final Color backgroundColour;
 
-  /**
-   * Class constructor. Specifies the tile's appearance.
-   * @param name
-   * @param glyph
-   * @param foregroundColour
-   * @param backgroundColour
-   */
-  public BackgroundTile(String name, char glyph, Color foregroundColour,
-      Color backgroundColour) {
+  public BackgroundTile(String name, char glyph, Color foregroundColour, @NonNull Color backgroundColour) {
     super(name, glyph, foregroundColour);
 
-    // Check for illegal arguments.
-    if (backgroundColour == null) {
-      throw new IllegalArgumentException("Background colour cannot be null.");
-    }
-
     this.backgroundColour = backgroundColour;
-  }
-
-  /**
-   * Gets the tile's background colour.
-   * @return
-   */
-  public Color getBackgroundColour() {
-    return backgroundColour;
   }
 }
