683189045c43e828f4b12fe8b155131e7c528f1b
Alex <alexghita96@gmail.com>
2021-01-09 22:30:53 +0000

    refactor: make `DisplayTile` standalone rather than a subclass of `BackgroundTile`

--- src/main/java/obscurum/display/DisplayTile.java
+++ src/main/java/obscurum/display/DisplayTile.java
@@ -1,39 +1,43 @@
 package obscurum.display;
 
 import java.awt.Color;
-import obscurum.environment.background.BackgroundTile;
 
-/**
- * This models a display tile, i.e. a tile that has no properties and is used
- * exclusively for display.
- * @author Alex Ghita
- */
-public class DisplayTile extends BackgroundTile {
-  /**
-   * Constructor specifying the tile's glyph.
-   * @param glyph
-   */
-  public DisplayTile(char glyph) {
-    this(glyph, Display.WHITE, Display.BLACK);
-  }
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import obscurum.GameMain;
 
-  /**
-   * Constructor specifying the tile's glyph and foreground colour.
-   * @param glyph
-   * @param foregroundColour
-   */
-  public DisplayTile(char glyph, Color foregroundColour) {
-    this(glyph, foregroundColour, Display.BLACK);
-  }
+@Getter
+public final class DisplayTile {
+    private final String name;
+    private final char glyph;
+    @Setter
+    private Color foregroundColour;
+    private final Color backgroundColour;
 
-  /**
-   * Constructor specifying the tile's glyph and colours.
-   * @param glyph
-   * @param foregroundColour
-   * @param backgroundColour
-   */
-  public DisplayTile(char glyph, Color foregroundColour,
-      Color backgroundColour) {
-    super("Display Tile", glyph, foregroundColour, backgroundColour);
-  }
+    public DisplayTile(char glyph) {
+        this(glyph, Display.WHITE, Display.BLACK);
+    }
+
+    public DisplayTile(char glyph, Color foregroundColour) {
+        this(glyph, foregroundColour, Display.BLACK);
+    }
+
+    public DisplayTile(char glyph, Color foregroundColour,
+                       Color backgroundColour) {
+        this("Display Tile", glyph, foregroundColour, backgroundColour);
+    }
+
+    public DisplayTile(@NonNull String name, char glyph, @NonNull Color foregroundColour, @NonNull Color backgroundColour) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty.");
+        } else if (glyph >= GameMain.NUM_OF_GLYPHS) {
+            throw new IllegalArgumentException("Glyph must be in range 0 - " + GameMain.NUM_OF_GLYPHS + ".");
+        }
+
+        this.name = name;
+        this.glyph = glyph;
+        this.foregroundColour = foregroundColour;
+        this.backgroundColour = backgroundColour;
+    }
 }
