34ef60b9e777eeba3166a7b9e6e49dd89746957c
Alex <alexghita96@gmail.com>
2021-01-09 23:20:26 +0000

    refactor: use new enum `DisplayCharacter` instead of raw char for `DisplayTile` glyph

--- src/main/java/obscurum/display/DisplayTile.java
+++ src/main/java/obscurum/display/DisplayTile.java
@@ -4,30 +4,23 @@ import java.awt.Color;
 
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.Setter;
-import obscurum.GameMain;
 
 @Getter
 public final class DisplayTile {
-    private final char glyph;
-    @Setter
-    private Color foregroundColour;
+    private final DisplayCharacter displayCharacter;
+    private final Color foregroundColour;
     private final Color backgroundColour;
 
-    public DisplayTile(char glyph) {
-        this(glyph, Display.WHITE, Display.BLACK);
+    public DisplayTile(@NonNull DisplayCharacter displayCharacter) {
+        this(displayCharacter, Display.WHITE, Display.BLACK);
     }
 
-    public DisplayTile(char glyph, Color foregroundColour) {
-        this(glyph, foregroundColour, Display.BLACK);
+    public DisplayTile(@NonNull DisplayCharacter displayCharacter, @NonNull Color foregroundColour) {
+        this(displayCharacter, foregroundColour, Display.BLACK);
     }
 
-    public DisplayTile(char glyph, @NonNull Color foregroundColour, @NonNull Color backgroundColour) {
-        if (glyph >= GameMain.NUM_OF_GLYPHS) {
-            throw new IllegalArgumentException("Glyph must be in range 0 - " + GameMain.NUM_OF_GLYPHS + ".");
-        }
-
-        this.glyph = glyph;
+    public DisplayTile(@NonNull DisplayCharacter displayCharacter, @NonNull Color foregroundColour, @NonNull Color backgroundColour) {
+        this.displayCharacter = displayCharacter;
         this.foregroundColour = foregroundColour;
         this.backgroundColour = backgroundColour;
     }
